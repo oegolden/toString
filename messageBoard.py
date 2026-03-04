618875414c4d13895f0cfb79be10ed4d77ff4933
oegolden <ogolden@g.hmc.edu>
2026-03-03 21:25:45 -0800

    sql db updated with things needed for messageBoard, messageBoard constructor created.

--- messageBoard.py
+++ messageBoard.py
@@ -1,19 +1,35 @@
+from os import name
 import sqlite3
+from utils.sqlService import get_pool
 
 class MessageBoard:
     def __init__(self, moderator, name):
+        try:
+            self.pool = get_pool()
+        except RuntimeError:
+             raise RuntimeError("Database pool not initialized. Please call init_pool() before creating a MessageBoard instance.")
         self.moderator = moderator
         self.name = name
-        self.messages = []
-        
-    def add_message(self, message):
-        self.messages.append(message)
+        self.messages = {}
+        with self.pool.connection() as conn:
+            cursor = conn.execute("SELECT message_id, message FROM messageBoard join messages on messageBoard.message_id = messages.message_id WHERE name = ? and moderator = ? AND status = 1", (name, moderator))
+            for row in cursor:
+                self.messages[row['message_id']] = row['message'] 
 
-    def get_messages(self):
+    async def add_message(self, message, message_id, user):
+        self.messages[message_id] = message
+        async with self.pool.connection() as conn:
+            await conn.execute("INSERT INTO messages (message_id, message, user, status) VALUES (?, ?, ?,1)", (message_id, message, user))
+            await conn.commit()
+
+    async def get_messages(self):
         return self.messages
     
-    def clear_messages(self, user, message_id):
+    async def clear_messages(self, user, message_id):
         if user == self.moderator:
-            self.messages.clear()
+            self.messages.pop(message_id, None)
+            async with self.pool.connection() as conn:
+                await conn.execute("DELETE FROM messages WHERE message_id = ?", (message_id,))
+                await conn.commit()
         else:
             user.send_message(f"You do not have permission to clear messages. Only the moderator '{self.moderator}' can clear messages.")
